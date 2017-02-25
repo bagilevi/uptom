783ef7072dd6970fb704d70899b51e3c8d14e814
Levente Bagi <bagilevi@gmail.com>
2017-02-25 12:40:15 +0000

    site manager

--- lib/uptom/checker.ex
+++ lib/uptom/checker.ex
@@ -1,16 +1,22 @@
 defmodule Uptom.Checker do
+  @moduledoc """
+    Executes a single check to the site and any associated behaviours,
+    like saving the result, sending up/down notifications.
+  """
   def check(site_id, url) do
     started_at = :calendar.universal_time()
 
-    {result, details} = Uptom.Pinger.ping(url)
+    {outcome, details} = result = Uptom.Pinger.ping(url)
 
     params = %{ site_id:     site_id,
                 started_at:  started_at,
-                success:     result == :ok,
+                success:     outcome == :ok,
                 status_code: details[:status_code],
                 message:     details[:message] }
 
     changeset = Uptom.SitePing.changeset(%Uptom.SitePing{}, params)
     Uptom.Repo.insert!(changeset)
+
+    result
   end
 end
---
+++ lib/uptom/site_manager.ex
@@ -0,0 +1,50 @@
+defmodule Uptom.SiteManager do
+  @moduledoc """
+    Long-running process that schedules checks.
+  """
+
+  use GenServer
+
+  def start_link(site_id, url, frequency) do
+    GenServer.start_link(
+      __MODULE__,
+      {site_id, url, frequency},
+      name: {:global, {:site_manager, site_id}}
+    )
+  end
+
+  def init({site_id, url, frequency}) do
+    schedule_work(frequency)
+    {:ok, {site_id, url, frequency}}
+  end
+
+  def handle_info(:work, {site_id, url, frequency}) do
+    schedule_work(frequency)
+
+    pid = spawn(fn ->
+      result = Uptom.Checker.check(site_id, url)
+      update_result(result)
+    end)
+    Process.monitor(pid)
+
+    {:noreply, {site_id, url, frequency}}
+  end
+
+  def handle_info({:DOWN, _, _, _, info} = message, state) do
+    case info do
+      :normal -> nil
+      _ ->
+        # IO.inspect message
+        update_result({:error, [message: "internal: checker failed"]})
+    end
+    {:noreply, state}
+  end
+
+  defp schedule_work(frequency) do
+    Process.send_after(self(), :work, frequency * 1000)
+  end
+
+  def update_result(result) do
+    IO.inspect result
+  end
+end
