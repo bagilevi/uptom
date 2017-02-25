8fbd8c32c67c09a139a6a353a3d6a793abef25f3
Levente Bagi <bagilevi@gmail.com>
2017-02-25 20:04:57 +0000

    redirect to sites when logged in

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule Uptom.PageController do
   use Uptom.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    if conn.assigns.current_user do
+      redirect(conn, to: site_path(conn, :index))
+    else
+      render conn, "index.html"
+    end
   end
 end
