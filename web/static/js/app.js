2bbe4f4e7f4dca2f039439791694f6ee6093273c
Levente Bagi <bagilevi@gmail.com>
2017-03-04 16:47:07 +0000

    show sites as cards

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,9 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+$(function() {
+  $('.clickable').on('click', function(ev){
+    window.location.href = $(this).data('href');
+  });
+});
